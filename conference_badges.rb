a44f0fc358c383ecfbab243c6a9e1b9ddd03d270
Patrick Mason <patrickmason73@gmail.com>
2023-05-26 21:30:41 -0600

    commit tree bf9e7ad85c06a364e1ec8d23b8298b477ecfea56

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(array)
+    array.map.with_index(1) do |name, index|
+        "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+end
+
+def printer(array)
+   batch_badge_creator(array).each do |badge|
+    puts badge
+    end
+    assign_rooms(array).each do |room|
+        puts room
+    end
+end
